9366485bc9af02776c2fba6f2ed31d0c76067d07
paulbhr <paulbh.roche@gmail.com>
2017-05-18 16:29:40 +0200

    maquette v0.8

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 window.onscroll = function() {scroll()};
 
 function scroll() {
-
+  let position = (document.getElementById('nav').offsetLeft - window.scrollX);
+  console.log(position);
   let x = document.documentElement.scrollTop;
   let fast = (x*2+25);
   let comingin = (x-700);
@@ -12,10 +13,12 @@ function scroll() {
       document.getElementById('nav').style.right = "";
       document.getElementById('nav').style.left = 0;
       document.getElementById("web").style.display = "flex";
+      document.getElementById("web").className = "animated fadeInUp";
   }
   else
   {
       document.getElementById("web").style.display = "none";
+      document.getElementById("web").className = "";
       document.getElementById('nav').style.left = "";
       document.getElementById('nav').style.right = ""+fast+"px";
       document.getElementById('edito').style.right = ""+fast+"px";
@@ -29,25 +32,72 @@ function scroll() {
   {
     document.getElementById('realisations').style.right = 0;
   }
+
+  let title = document.getElementsByTagName('h1');
+
+  for(i=0; i < title.length; i+=2) {
+    title[i].style.transition = "box-shadow 0.5s";
+    let position = (title[i].offsetTop - window.scrollY);
+    if ((document.body.offsetHeight - position) > (document.body.offsetHeight / 2)) {
+      title[i].classList.add("boxShadowLeft");
+    }
+    else {
+      title[i].classList.remove("boxShadowLeft");
+    }
+  }
+  for(i=1; i < title.length; i+=2) {
+    title[i].style.transition = "box-shadow 0.5s";
+    let position = (title[i].offsetTop - window.scrollY);
+    if ((document.body.offsetHeight - position) > (document.body.offsetHeight / 2)) {
+      title[i].classList.add("boxShadowRight");
+    }
+    else {
+      title[i].classList.remove("boxShadowRight");
+    }
+  }
 }
 
-function progress() {
-    let p = document.getElementById('competences').getElementsByTagName('p');
-    let width = 0;
-    let id = setInterval(frame, 10);
-    function frame() {
-      width++;
-      for (i = 0; i < p.length; i++) {
-        p[i].style.width = 0;
-        if (width >= p[i].className) {
-          p[i].style.width = p[i].className + '%';
-        }
-        else {
-          p[i].style.width = width + '%';
-        }
+function progress(element) {
+  element.nextElementSibling.className = "animated fadeInRight"
+  let p = document.getElementById('competences').getElementsByTagName('p');
+  let width = 0;
+  let id = setInterval(frame, 10);
+  function frame() {
+    width++;
+    for (i = 0; i < p.length; i++) {
+      p[i].style.width = 0;
+      if (width >= p[i].className) {
+        p[i].style.width = p[i].className + '%';
       }
-      if (width == 100) {
-        clearInterval(id);
+      else {
+        p[i].style.width = width + '%';
       }
     }
+    if (width == 100) {
+      clearInterval(id);
+    }
+  }
+}
+
+function slidein(element) {
+  if (element.nextElementSibling.className = "ng-hide") {
+    element.nextElementSibling.className = "animated fadeInRight";
+  }
+  else if(element.nextElementSibling.className = "ng-show") {
+    element.nextElementSibling.className = "animated fadeOut";
+  }
+}
+
+function article(element) {
+  if (element.lastElementChild.className = "ng-hide") {
+    element.lastElementChild.className = "animated fadeInLeft";
+  }
+  else if(element.lastElementChild.className = "ng-show") {
+    element.lastElementChild.className = "animated fadeOut";
+  }
+}
+
+function scrollto(id) {
+  let element = document.getElementById(id);
+  element.scrollIntoView();
 }
